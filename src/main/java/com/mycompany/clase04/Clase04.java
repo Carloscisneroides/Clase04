7e223f72bb2d5befa6b3a2f1520cebfa2d8aed0e
camper <camper@artemis-9048-900-0017>
2025-09-04 14:17:54 -0500

    prueba

---
+++ src/main/java/com/mycompany/clase04/Clase04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase04;
+
+/**
+ *
+ * @author camper
+ */
+public class Clase04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
